c5f360ac834bf95bd2530ec08fca74ae529cfb4d
CatherineStone <106274539+CatherineStone@users.noreply.github.com>
2023-03-08 13:06:20 +0000

    final changes

--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 5001;
 
 const Book = require("./books/model");
 const Author = require("./authors/model");
+const Genre = require("./genres/model")
 
 const bookRouter = require("./books/routes");
 const authorRouter = require("./authors/routes");
+const genreRouter = require("./genres/routes")
 
 const app = express();
 
@@ -15,15 +17,20 @@ app.use(express.json());
 
 const syncTables = () => {
 
+    Genre.hasMany(Book);
+    Book.belongsTo(Genre);
+
     Author.hasMany(Book);
     Book.belongsTo(Author);
 
     Book.sync({ alter: true });
     Author.sync({ alter: true });
+    Genre.sync({ alter: true })
 };
 
 app.use(bookRouter);
 app.use(authorRouter);
+app.use(genreRouter);
 
 app.get("/health", (req, res) => 
 res.status(200).json({ message: "API is working" }));
